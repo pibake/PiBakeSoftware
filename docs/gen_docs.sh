48cce1d96f4afb3de6488fedc23d27b6594bb3cb
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-03-13 21:10:14 -0400

    fleshed out travis.yml

--- docs/gen_docs.sh
+++ docs/gen_docs.sh
@@ -1,9 +1,14 @@
 # Make sure you install modules in requirements.txt and Rust (cargo) before you run this
 # If you don't install requirements.txt, you'll get errors
 
-rm -rf ../pibakesoftware/html
-rm -rf ../uuid/target
-cd ../pibakesoftware
-pdoc --html connect.py write.py util.py temperature.py
-cd ../uuid
-cargo doc --no-deps --open
+rm -rf pibakesoftware/html
+rm -rf uuid/target
+pdoc --html --overwrite pibakesoftware/__init__.py
+#pdoc --html --overwrite pibakesoftware/__main__.py
+pdoc --html --overwrite pibakesoftware/connect.py
+#pdoc --html --overwrite pibakesoftware/read.py
+pdoc --html --overwrite pibakesoftware/temperature.py
+pdoc --html --overwrite pibakesoftware/write.py
+pdoc --html --overwrite pibakesoftware/util.py
+cd uuid
+cargo doc --no-deps
