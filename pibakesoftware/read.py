20d3386b9b2c6b975353b96e8f188dcc8175f417
Wyatt Miller <wjmiller2016@gmail.com>
2019-04-02 21:49:46 -0400

    refactored reading logic and temperature serializer

--- pibakesoftware/read.py
+++ pibakesoftware/read.py
@@ -2,7 +2,10 @@
 # Written by Wyatt J. Miller, 2019
 
 import math
-import grovepi
+import os
+import glob
+import time
+
 from . import temperature
 
 
@@ -11,20 +14,62 @@ class Read:
     Class to read temperature and humidity off of the Raspberry Pi temperature sensor
     '''
 
-    def __init__(self, temp_sensor, humid_sensor):
+    def __init__(self):
         '''
         Constructor
         '''
-        self.temp_sensor = temp_sensor
-        self.humid_sensor = humid_sensor
+        self.temp_c = 0
+        self.temp_f = 0
 
+        self.base_dir = '/sys/bus/w1/devices/'
+        self.device_folder = glob.glob(self.base_dir + '28*')[0]
+        self.device_file = self.device_folder + '/w1_slave'
+
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-therm')
+
+    def read_temp_raw(self):
+        '''
+        TODO: Needs documentation
+        '''
+        f = open(self.device_file, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
+
+    def read_temp(self):
+        '''
+        TODO: Needs documentation
+        '''
+        lines = self.read_temp_raw()
+
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = self.read_temp_raw()
+        
+        equals_pos = lines[1].find('t=')
+        
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+        
+            result = temperature.Temperature(temp_c, temp_f)
+            return result
+    
     def read_temperature(self):
         '''
         Method to read temperature and humidity
+
+        NOTE: DEPRECATED, DO NOT USE
         '''
         try:
             # store temp and humid in variables
-            [temp, hum] = grovepi.dht(self.temp_sensor, self.humid_sensor)
+            # [temp, hum] = grovepi.dht(self.temp_sensor, self.humid_sensor)
+
+            # placeholder stuff
+            temp = 0
+            hum = 0
 
             # check if we have nans
             # if so, then raise a type error exception
@@ -44,4 +89,6 @@ class Read:
 
         except KeyboardInterrupt as e:
             print(str(e))
+
+
         
