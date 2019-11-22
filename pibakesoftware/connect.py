b48bcba8c0dc2f45226e8957edad385aff14fb0d
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-11-22 13:00:31 -0500

    changed from ssh/sftp to POST request

--- pibakesoftware/connect.py
+++ pibakesoftware/connect.py
@@ -3,8 +3,7 @@
 
 import os
 import sys
-import pysftp
-from paramiko import SSHClient, AutoAddPolicy
+import requests
 
 class Connect:
     '''
@@ -16,7 +15,9 @@ class Connect:
     and connects to server through SSH and SFTP
     '''
 
-    def __init__(self, file, host, port, username, password, local_path):
+    def __init__(self, file, host, port, path):
+        # TODO: Revise documentation for this constructor
+        
         '''
         Synopsis:
         Constructor for the Connect class
@@ -26,12 +27,12 @@ class Connect:
         '''
         self.file = file
         self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
-        self.local_path = local_path
+        self.port = str(port)
+        self.path = path
 
     def connect_to_server(self):
+        # TODO: Revise documentation for this method
+        
         '''
         Synopsis:
         Method to connect via SFTP/SSH
@@ -44,26 +45,14 @@ class Connect:
         The SFTP connection sends self.file and closes
         '''
 
-        try:
-            ssh = SSHClient()
-            ssh.set_missing_host_key_policy(AutoAddPolicy())
-            ssh.load_system_host_keys()
+        url = f"http://{self.host}:{self.port}{self.path}"
+        json = self.file
+        headers = {"Content-Type":"application/json"}
+        response = requests.post(url=url, data=json, headers=headers)
+        result = response.status_code
 
-            cnopts = pysftp.CnOpts()
-            cnopts.hostkeys = None
-
-            sftp = pysftp.Connection(self.host, username=self.username, password=self.password, cnopts=cnopts)
-            sftp.put(self.file)
-
-            ssh.connect(self.host, port=self.port, username=self.username, password=self.password)
-            ssh.exec_command("php import.php")
-
-            ssh.close()
-            sftp.close()
-
-            return True
-
-        except Exception:
-            print("Exception occurred, returning false!")
+        if result != 200:
             return False
+
+        return True
 
